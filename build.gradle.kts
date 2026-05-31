252576e0af2af2cd412feb54408d72fc541fe722
fascinated <liam@fascinated.cc>
2026-05-31 04:04:09 +0100

    dont need all of these

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,9 +25,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
 
-    // Websockets
-    implementation("org.springframework.boot:spring-boot-starter-websocket")
-
     // Postgres
     implementation("org.flywaydb:flyway-core")
     implementation("org.flywaydb:flyway-database-postgresql")
@@ -39,39 +36,21 @@ dependencies {
 
     // Libraries
     implementation("com.pngencoder:pngencoder:0.16.0")
-    compileOnly("org.projectlombok:lombok:1.18.42")
-    annotationProcessor("org.projectlombok:lombok:1.18.42")
-    implementation("com.google.code.gson:gson:2.14.0")
-    implementation("net.jodah:expiringmap:0.5.11")
     implementation("org.apache.httpcomponents.client5:httpclient5:5.6.1")
     implementation("org.apache.httpcomponents.core5:httpcore5:5.4.2")
     implementation("com.squareup.okhttp3:okhttp-urlconnection:5.3.2")
-    implementation("org.kohsuke:github-api:2.0-rc.6")
-
-    // Adventure API
-    implementation("net.kyori:adventure-text-serializer-gson:4.26.1")
-    implementation("net.kyori:adventure-text-serializer-legacy:4.26.1")
-    implementation("com.google.guava:guava:33.5.0-jre")
-
-    // DNS Lookup
-    implementation("com.github.dnsjava:dnsjava:v3.5.2")
-
-    // SwaggerUI
-    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.0.2")
 
     // GeoIP - IP Lookups
     implementation("com.maxmind.geoip2:geoip2:5.0.2")
-
-    // Archive Utilities
-    implementation("org.codehaus.plexus:plexus-archiver:4.11.0")
-
-    // File Storage
-    implementation("io.minio:minio:8.6.0")
 
     // Metrics
     implementation("io.prometheus:prometheus-metrics-core:1.6.1")
     implementation("io.prometheus:prometheus-metrics-exposition-formats:1.6.1")
     implementation("io.prometheus:prometheus-metrics-model:1.6.1")
+
+    // Lombok
+    compileOnly("org.projectlombok:lombok:1.18.42")
+    annotationProcessor("org.projectlombok:lombok:1.18.42")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
