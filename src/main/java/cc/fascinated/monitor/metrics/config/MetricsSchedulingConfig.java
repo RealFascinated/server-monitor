9c62210ef61a9d19005a42622d24acb3f3fab357
fascinated <liam@fascinated.cc>
2026-06-13 01:07:21 +0100

    fix?

--- src/main/java/cc/fascinated/monitor/metrics/config/MetricsSchedulingConfig.java
+++ src/main/java/cc/fascinated/monitor/metrics/config/MetricsSchedulingConfig.java
@@ -1,11 +1,17 @@
 package cc.fascinated.monitor.metrics.config;
 
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
 @EnableScheduling
-@EnableConfigurationProperties(MonitorMetricsProperties.class)
 public class MetricsSchedulingConfig {
+
+    @Bean
+    @ConfigurationProperties(prefix = "monitor.metrics")
+    public MonitorMetricsProperties monitorMetricsProperties() {
+        return new MonitorMetricsProperties();
+    }
 }
